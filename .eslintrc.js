d0599ebf45f7c4240d2edf9565a2ee453da5c18d
moroz-art-dev <moroz.art.dev@gmail.com>
2024-08-16 20:32:52 +0300

    Default configs + new packages

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  plugins: ['import', '@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '*.module.scss',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
---
+++ next.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  trailingSlash: false,
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, 'components'),
+      '@config': path.resolve(__dirname, 'config'),
+      '@containers': path.resolve(__dirname, 'containers'),
+      '@contexts': path.resolve(__dirname, 'contexts'),
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@types': path.resolve(__dirname, 'types'),
+      '@styles': path.resolve(__dirname, 'styles'),
+    };
+    return config;
+  },
+};
+
+export default withNextIntl(nextConfig);
