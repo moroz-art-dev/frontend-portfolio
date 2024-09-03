df2b599c9288209cf84bb1ec898e281c35024bdf
moroz-art-dev <moroz.art.dev@gmail.com>
2024-09-03 18:22:46 +0300

    update useCustomTheme()

--- src/containers/ContactContainer/ContactContainer.tsx
+++
@@ -1,3 +0,0 @@
-export default function ContactContainer() {
-  return <></>;
-}
--- src/hooks/useCustomTheme.ts
+++ src/hooks/useCustomTheme.ts
@@ -43,78 +43,77 @@ export const useCustomTheme = () => {
       code: codeFont,
     },
     styles: {
-      global: (props: GlobalStyleProps) => ({
-        ':root': {
-          '--primary-color': props.theme.colors[props.colorMode].primary,
-          '--secondary-color': props.theme.colors[props.colorMode].secondary,
-          '--accent-color': props.theme.colors[props.colorMode].accent,
-          '--accent-hover-color':
-            props.theme.colors[props.colorMode].accentHover,
-          '--accent-active-color':
-            props.theme.colors[props.colorMode].accentActive,
-          '--background-color': props.theme.colors[props.colorMode].background,
-          '--text-color': props.theme.colors[props.colorMode].text,
-          '--button-color': props.theme.colors[props.colorMode].button,
-          '--button-hover-color':
-            props.theme.colors[props.colorMode].buttonHover,
-          '--button-active-color':
-            props.theme.colors[props.colorMode].buttonActive,
-          '--link-color': props.theme.colors[props.colorMode].link,
-          '--link-hover-color': props.theme.colors[props.colorMode].linkHover,
-          '--link-active-color': props.theme.colors[props.colorMode].linkActive,
-          '--border-color': props.theme.colors[props.colorMode].border,
-          '--header-background':
-            props.theme.colors[props.colorMode].headerBackground,
-          '--shadow-color': props.theme.colors[props.colorMode].shadow,
-          '--shadow-header-color':
-            props.theme.colors[props.colorMode].headerShadow,
-          '--tag-bg': props.theme.colors[props.colorMode].accent,
-          '--padding-background': space.backgroundPadding,
-          '--height-header': sizes.headerHeight,
-          '--font-heading': headingFont,
-          '--font-body': bodyFont,
-          '--font-special': specialFont,
-          '--font-alternate': alternateFont,
-          '--font-code': codeFont,
-        },
-        body: {
-          bg: props.theme.colors[props.colorMode].button,
-          color: props.theme.colors[props.colorMode].text,
-          fontFamily: props.theme.fonts.body,
-          fontSize: {
-            base: '14px',
-            md: '14px',
-            lg: '16px',
-            xl: '18px',
+      global: (props: GlobalStyleProps) => {
+        const currentColors =
+          props.theme.colors[props.colorMode] || lightPalette.colors;
+
+        return {
+          ':root': {
+            '--primary-color': currentColors.primary,
+            '--secondary-color': currentColors.secondary,
+            '--accent-color': currentColors.accent,
+            '--accent-hover-color': currentColors.accentHover,
+            '--accent-active-color': currentColors.accentActive,
+            '--background-color': currentColors.background,
+            '--text-color': currentColors.text,
+            '--button-color': currentColors.button,
+            '--button-hover-color': currentColors.buttonHover,
+            '--button-active-color': currentColors.buttonActive,
+            '--link-color': currentColors.link,
+            '--link-hover-color': currentColors.linkHover,
+            '--link-active-color': currentColors.linkActive,
+            '--border-color': currentColors.border,
+            '--header-background': currentColors.headerBackground,
+            '--shadow-color': currentColors.shadow,
+            '--shadow-header-color': currentColors.headerShadow,
+            '--tag-bg': currentColors.accent,
+            '--padding-background': space.backgroundPadding,
+            '--height-header': sizes.headerHeight,
+            '--font-heading': headingFont,
+            '--font-body': bodyFont,
+            '--font-special': specialFont,
+            '--font-alternate': alternateFont,
+            '--font-code': codeFont,
           },
-          lineHeight: {
-            // base: '1.5',
-            // md: '1.6',
-            // lg: '1.7',
-            // xl: '1.8',
+          body: {
+            bg: currentColors.button,
+            color: currentColors.text,
+            fontFamily: props.theme.fonts.body,
+            fontSize: {
+              base: '14px',
+              md: '14px',
+              lg: '16px',
+              xl: '18px',
+            },
+            lineHeight: {
+              // base: '1.5',
+              // md: '1.6',
+              // lg: '1.7',
+              // xl: '1.8',
+            },
           },
-        },
-        h1: {
-          fontSize: {base: '2xl', sm: '2xl', md: '3xl', lg: '4xl'},
-          fontWeight: 600,
-          textTransform: 'uppercase',
-          '::first-letter': {
-            color: 'var(--button-active-color)',
+          h1: {
+            fontSize: {base: '2xl', sm: '2xl', md: '3xl', lg: '4xl'},
+            fontWeight: 600,
+            textTransform: 'uppercase',
+            '::first-letter': {
+              color: 'var(--button-active-color)',
+            },
           },
-        },
-        h2: {
-          fontSize: {base: 'xl', sm: 'xl', md: '2xl', lg: '3xl'},
-          fontWeight: 400,
-        },
-        h3: {
-          fontSize: {base: 'md', sm: 'md', md: 'lg', lg: '2xl'},
-          fontWeight: 400,
-        },
-        h4: {
-          fontSize: {base: 'sm', sm: 'sm', md: 'md', lg: 'lg'},
-          fontWeight: 400,
-        },
-      }),
+          h2: {
+            fontSize: {base: 'xl', sm: 'xl', md: '2xl', lg: '3xl'},
+            fontWeight: 400,
+          },
+          h3: {
+            fontSize: {base: 'md', sm: 'md', md: 'lg', lg: '2xl'},
+            fontWeight: 400,
+          },
+          h4: {
+            fontSize: {base: 'sm', sm: 'sm', md: 'md', lg: 'lg'},
+            fontWeight: 400,
+          },
+        };
+      },
     },
     components: {
       Button,
